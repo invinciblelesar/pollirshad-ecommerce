b9e88bfcc2a9e6fccad5b3505a5548a1538782f3
invinciblelesar <invinciblelesar@gmail.com>
2026-03-26 00:29:46 +0600

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,29 @@
+const CACHE_NAME = 'pollirshad-store-v2'; // Updated to v2 to force clear old cache
+const ASSETS_TO_CACHE =[
+    '/', 
+    '/index.html', 
+    '/admin.html',
+    '/app.js', 
+    '/manifest.json'
+];
+
 self.addEventListener('install', (e) => {
     e.waitUntil(
-        caches.open('pollirshad-store').then((cache) => cache.addAll([
-            '/', '/index.html', '/app.js', '/manifest.json'
-        ]))
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+// This activate event clears out the old cache (v1) when the new service worker takes over
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('✅ Clearing old cache:', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
     );
 });
 
